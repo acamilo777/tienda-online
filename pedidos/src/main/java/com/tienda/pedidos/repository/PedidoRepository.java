839f0d608311ceac9cb192ec9797d2f6f44b6cf6
acamilo777 <am.artiaga@duocuc.cl>
2025-06-24 00:03:21 -0400

    Experiencia 3

--- pedidos/src/main/java/com/tienda/pedidos/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tienda.pedidos.repository;
-
-import com.tienda.pedidos.entities.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
--- productos/src/main/java/com/tienda/productos/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tienda.productos.repository;
-
-import com.tienda.productos.entities.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
